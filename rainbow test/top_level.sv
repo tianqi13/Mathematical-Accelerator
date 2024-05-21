c3f75ff617788839b42b9ee118c1fb9a1307e39e
Constance <constancedelamarliere@gmail.com>
2024-05-21 15:51:43 +0100

    overall mod

--- rainbow test/top_level.sv
+++ rainbow test/top_level.sv
@@ -26,11 +26,20 @@ Pixel pixel(
     .Y(y) 
 );
 
+logic [19:0] rd_addr;
+Addr_counter addr_counter(
+    .clk(clk),
+    .rst(rst),
+    .en(ovf),
+    .addr_counter(rd_addr)
+);
+
+
 Ram ram(
     .clk(clk),
     .WRITE_EN(~ovf),
     .READ_EN(ovf),
-    .rd_addr(),
+    .rd_addr(rd_addr),
     .wr_addr({y,x}),
     .din(count_out),
     .out(RGB_out)
