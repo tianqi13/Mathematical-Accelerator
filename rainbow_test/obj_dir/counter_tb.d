8a58623cc356c53e802358965388b89f7ae4c09d
constancegdl <147669242+constancegdl@users.noreply.github.com>
2024-05-22 11:04:31 +0100

    delete

--- rainbow_test/obj_dir/counter_tb.d
+++
@@ -1,9 +0,0 @@
-counter_tb.o: ../counter_tb.cpp Vtop_level.h \
-  /usr/local/Cellar/verilator/5.016/share/verilator/include/verilated.h \
-  /usr/local/Cellar/verilator/5.016/share/verilator/include/verilatedos.h \
-  /usr/local/Cellar/verilator/5.016/share/verilator/include/verilated_config.h \
-  /usr/local/Cellar/verilator/5.016/share/verilator/include/verilated_types.h \
-  /usr/local/Cellar/verilator/5.016/share/verilator/include/verilated_funcs.h \
-  /usr/local/Cellar/verilator/5.016/share/verilator/include/verilated_vcd_c.h \
-  /usr/local/Cellar/verilator/5.016/share/verilator/include/verilated_trace.h \
-  /usr/local/Cellar/verilator/5.016/share/verilator/include/verilated_trace_defs.h
