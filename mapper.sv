c9e7f3036c5e375f5f7b39411ea4760719ed7d01
oliver905 <oliverhannibal80@gmail.com>
2024-05-23 17:25:31 +0100

    updates

--- mapper.sv
+++ mapper.sv
@@ -4,18 +4,32 @@ module mapper(
     input logic clk,
     input logic rst,
     input logic en,
-    input logic [9:0]  X,
-    input logic [9:0]  Y,
     input logic [31:0] delta_x,
     input logic [31:0] delta_y,
     input logic [31:0] re_axis_width,
     input logic [31:0] im_axis_width,
-    output logic [31:0] a,  // real coordinate
-    output logic [31:0] b   // imag coordinate
+    output logic [31:0] real,
+    output logic [31:0] ims,
+    output logic        ovf,
+    output logic [31:0] x,
+    output logic [31:0] y,
 );
 
+logic [9:0]  X,
+assign x = {1'b0, X, 21'b0};
+logic [9:0]  Y,
+assign y = {1'b0, Y, 21'b0};
 logic [31:0] XtimesDelta;
 logic [31:0] YtimesDelta;
+
+dimensions DIMENSIONS(
+    .clk(clk);
+    .en(en);
+    .rst(rst);
+    .OVF(ovf);
+    .X(X);
+    .Y(Y);
+)
 
 multiplier XtimesDELTA(
     .clk(clk);
