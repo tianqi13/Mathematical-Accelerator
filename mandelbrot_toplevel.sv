483a45c1ef07e5d7f5a2cd47e65cded510f01596
oliver905 <oliverhannibal80@gmail.com>
2024-05-23 16:59:56 +0100

    to push

--- mandelbrot_toplevel.sv
+++ mandelbrot_toplevel.sv
@@ -2,12 +2,25 @@ module mandelbrot_toplevel.sv(
     input logic rst,
     input logic clk,
     input logic en,
-    input logic x_size,
-    input logic y_size,
+    input logic [9:0] x_size,
+    input logic [9:0] y_size,
+    input logic [31:0] re_axis_width,
+    input logic [31:0] im_axis_width,
 );
 
-mapper MAPPER (
+logic [31:0] delta = re_axis_width/x_size;
+logic [31:0] a,  // real coordinate
+logic [31:0] b   // imag coordinate
 
+mapper MAPPER (
+    .clk(clk);
+    .X(x_size);
+    .Y(y_size);
+    .delta(delta);
+    .re_axis_width(re_axis_width);
+    .im_axis_width(im_axis_width);
+    .a(a);  // real coordinate
+    .b(b);   // imag coordinate
 )
 
 endmodule
