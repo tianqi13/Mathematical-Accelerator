1971eb91a5e87de27cf01d214b29694838ca1bae
constancegdl <147669242+constancegdl@users.noreply.github.com>
2024-05-23 15:37:56 +0100

    ram added to top

--- mandelbrot_toplevel.sv
+++ mandelbrot_toplevel.sv
@@ -10,4 +10,24 @@ mapper MAPPER (
 
 )
 
+logic [19:0] rd_addr;
+Addr_counter addr_counter(
+    .clk(clk),
+    .rst(rst),
+    .en(ovf),
+    .addr_count(rd_addr),
+    .X(X),
+    .Y(Y)
+);
+
+Ram ram(
+    .clk(clk),
+    .WRITE_EN(~ovf),
+    .READ_EN(ovf),
+    .rd_addr(rd_addr),
+    .wr_addr({y,x}),
+    .din(count_out),  // input
+    .RGB(RGB_out)
+);
+
 endmodule
