8575c9a86047e1096bb1ee28a6eb27d9f4177e4f
oliver905 <oliverhannibal80@gmail.com>
2024-05-25 12:40:03 +0100

    updates

--- multiplier.sv
+++ multiplier.sv
@@ -1,14 +1,12 @@
 // This module multiplies 2 32 bit numbers and truncates the result to produce a 32 bit output.
 
-module multiplier #(
-    parameter WIDTH = 32
-)(
-    input  logic [WIDTH-1:0] a,   // assuming form S,BBBBBBBBBB.DDDDDDDDDDDDDDDDDDDDD (1 sign, 10 bits of data, and 21 decimals)
-    input  logic [WIDTH-1:0] b,
-    output logic [WIDTH-1:0] result
+module multiplier(
+    input  logic [31:0] a,   // assuming form S,BBBBBBBBBB.DDDDDDDDDDDDDDDDDDDDD (1 sign, 10 bits of data, and 21 decimals)
+    input  logic [31:0] b,
+    output logic [31:0] result
 );
 
-logic [2*WIDTH-1:0] intermediate;
+logic [63:0] intermediate;
 
 assign intermediate = a*b;
 assign result = intermediate[52:21];
